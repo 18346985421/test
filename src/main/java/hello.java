28b32989e365304dd566893f5f7ffc7be7c5040f
tyler <tyler@163.com>
2020-05-10 16:53:46 +0800

    first commit

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author time_
+ * @Date 2020/5/1016:38
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("太难了");
+    }
+}
